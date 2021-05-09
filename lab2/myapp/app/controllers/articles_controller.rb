1b07b451ac850e34fffb9ec7e8cfc06e256034b7
MohammedHieba <mohamedhieba9574@gmail.com>
2021-05-09 20:39:53 +0200

    lab3

--- lab2/myapp/app/controllers/articles_controller.rb
+++ lab2/myapp/app/controllers/articles_controller.rb
@@ -1,9 +1,63 @@
 class ArticlesController < ApplicationController
+
+
+  #list of articles
+  def index
+    @articles = Article.all
+  end
+
+  # create new article
   def new
-      @name = params["name"]
+    @name = params['name']
+    @article = Article.new
+    
   end
 
   def create
-      render plain: params[:article].inspect
+    @article = Article.new(article_params)
+    @article.save
+    if @article.save
+      redirect_to articles_path
+    else
+      render 'new'
+    end
+  end
+   
+
+  #edit article
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to articles_path
+    else
+      render 'edit'
+    end
+  end
+
+
+  #show article
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  # delete an article
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to articles_path
+  end
+
+
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :body)
   end
 end
--- lab2/myapp/config/routes.rb
+++ lab2/myapp/config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+
+  # devise_for :users
+  root to: 'welcome#index'
   get 'welcome/index'
- 
-  resources :articles
- 
-  root 'welcome#index'
+
+  resources :articles do
+    resources :comments
+  end
 end
